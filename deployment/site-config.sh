686feef98d37a5e79b0d2c21bfae5e4db719f728
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2024-09-19 14:44:25 +0200

    solving SHELL_SHFMT issue

--- deployment/site-config.sh
+++ deployment/site-config.sh
@@ -6,18 +6,18 @@ set -e
 # Reads from config.yaml the clouds to use
 
 dump_config() {
-    # dumps a piece of yaml ready to be included in the
-    # clouds.yaml Openstack client config
-    cloud_name="$1"
-    site="$2"
-    vo="$3"
-    oidc_token="$4"
-    token="$(fedcloud openstack token issue \
-                      --oidc-access-token "$oidc_token" \
-                      --site "$site" --vo "$vo" -j \
-                      | jq -r '.[0].Result.id')"
-    eval "$(fedcloud site show-project-id --site "$site" --vo "$vo")"
-    cat << EOF
+	# dumps a piece of yaml ready to be included in the
+	# clouds.yaml Openstack client config
+	cloud_name="$1"
+	site="$2"
+	vo="$3"
+	oidc_token="$4"
+	token="$(fedcloud openstack token issue \
+		--oidc-access-token "$oidc_token" \
+		--site "$site" --vo "$vo" -j |
+		jq -r '.[0].Result.id')"
+	eval "$(fedcloud site show-project-id --site "$site" --vo "$vo")"
+	cat <<EOF
   $cloud_name:
     auth_type: token
     auth:
@@ -30,16 +30,16 @@ EOF
 # using OIDC_TOKEN generated in .github/workflows/deploy.yaml
 
 rm -f clouds.yaml
-echo "clouds:" > tmp-clouds.yaml
+echo "clouds:" >tmp-clouds.yaml
 dump_config backend \
-            "$(yq -r .clouds.backend.site config.yaml)" \
-            "$(yq -r .clouds.backend.vo config.yaml)" \
-            "$OIDC_TOKEN" >> tmp-clouds.yaml
+	"$(yq -r .clouds.backend.site config.yaml)" \
+	"$(yq -r .clouds.backend.vo config.yaml)" \
+	"$OIDC_TOKEN" >>tmp-clouds.yaml
 
 dump_config deploy \
-            "$(yq -r .clouds.deploy.site config.yaml)" \
-            "$(yq -r .clouds.deploy.vo config.yaml)" \
-            "$OIDC_TOKEN" >> tmp-clouds.yaml
+	"$(yq -r .clouds.deploy.site config.yaml)" \
+	"$(yq -r .clouds.deploy.vo config.yaml)" \
+	"$OIDC_TOKEN" >>tmp-clouds.yaml
 
 mv tmp-clouds.yaml clouds.yaml
 mkdir -p ~/.config/openstack
