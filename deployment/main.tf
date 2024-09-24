d8ab8e8c865d936d3972b4895bc0be8281cb27c4
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2024-09-24 11:30:20 +0200

    enable back motley-cue

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,13 +1,12 @@
 resource "openstack_compute_instance_v2" "dashboard" {
   name      = "dashboard"
-  image_id  = var.image_id
+  image_id  = data.openstack_images_image_v2.egi-docker.id
   flavor_id = var.flavor_id
   user_data = file("cloud-init.yaml")
   network {
     uuid = var.net_id
   }
-  security_groups = ["HTTP"]
-  //security_groups = ["HTTP", "motley-cue"]
+  security_groups = ["HTTP", "motley-cue"]
 }
 
 resource "openstack_compute_secgroup_v2" "secgroup" {
@@ -29,25 +28,25 @@ resource "openstack_compute_secgroup_v2" "secgroup" {
   }
 }
 
-//resource "openstack_compute_secgroup_v2" "motley" {
-//  name        = "motley-cue"
-//  description = "Open access via ssh-oidc"
-//
-//  rule {
-//    from_port   = 22
-//    to_port     = 22
-//    ip_protocol = "tcp"
-//    cidr        = "0.0.0.0/0"
-//  }
-//
-//  rule {
-//    from_port   = 8181
-//    to_port     = 8181
-//    ip_protocol = "tcp"
-//    cidr        = "0.0.0.0/0"
-//  }
-//
-//}
+resource "openstack_compute_secgroup_v2" "motley" {
+  name        = "motley-cue"
+  description = "Open access via ssh-oidc"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8181
+    to_port     = 8181
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+}
 
 resource "openstack_networking_floatingip_v2" "fip" {
   pool = var.ip_pool
