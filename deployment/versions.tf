44b964ac905270d6d9c3e95510a6f58b7da334ab
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2024-09-25 15:01:53 +0200

    see https://github.com/terraform-provider-openstack/terraform-provider-openstack/blob/main/docs/guides/upgrade-guide-version-3.md

--- deployment/versions.tf
+++ deployment/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     openstack = {
       source = "terraform-provider-openstack/openstack"
+      version = "~> 2.1.0"
     }
   }
-  required_version = ">= 0.13"
 }
